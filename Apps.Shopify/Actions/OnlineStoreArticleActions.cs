b1ef43e51e312b6f3bbd9b2a73a464dc93af8db7
Cecilia Nistal <cecilia.nistal@centific.com>
2024-05-30 22:42:44 -0300

    Adding actions

--- Apps.Shopify/Actions/OnlineStoreArticleActions.cs
+++ Apps.Shopify/Actions/OnlineStoreArticleActions.cs
@@ -1,11 +1,19 @@
 using Apps.Shopify.Actions.Base;
+using Apps.Shopify.Constants.GraphQL;
+using Apps.Shopify.Models.Entities;
 using Apps.Shopify.Models.Request;
 using Apps.Shopify.Models.Request.OnlineStoreArticle;
 using Apps.Shopify.Models.Response;
+using Apps.Shopify.Models.Response.Article;
+using Apps.Shopify.Models.Response.Page;
+using Apps.Shopify.Models.Response.TranslatableResource;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using System.ComponentModel;
+using System.Security.AccessControl;
+using System.Threading;
 
 namespace Apps.Shopify.Actions;
 
@@ -15,6 +23,27 @@ public class OnlineStoreArticleActions : TranslatableResourceActions
     public OnlineStoreArticleActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient) :
         base(invocationContext, fileManagementClient)
     {
+    }
+
+    [Action("List online store articles", Description = "List all aricles for the online store")]
+    public async Task<ListArticlesResponse> ListArticles() 
+    {
+        var variables = new Dictionary<string, object>()
+        {
+            ["resourceType"] = TranslatableResource.ONLINE_STORE_ARTICLE
+        };
+        var response = await Client
+            .Paginate<TranslatableResourceEntity, TranslatableResourcePaginationResponse>(
+                GraphQlQueries.TranslatableResources,
+                variables);
+        return new ListArticlesResponse
+        {
+            Articles = response.Select(x => new Article
+            {
+                ResourceId = x.ResourceId,
+                Title = x.TranslatableContent.First(y => y.Key == "title").Value
+            })
+        };
     }
 
     [Action("Get online store article translation as HTML",
