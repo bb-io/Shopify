ffcc910501e6bd89158c6f4aa10225e17464dfad
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-28 16:38:02 +0200

    Added error handling

---
+++ Tests.Shopify/ProductTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.Shopify.Actions;
+using Apps.Shopify.Models.Request.Product;
+using ShopifyTests.Base;
+
+namespace Tests.Shopify
+{
+    [TestClass]
+    public class ProductTests : TestBase
+    {
+        [TestMethod]
+        public async Task SearchProduct_IsSuccess()
+        {
+            var actions = new ProductActions(InvocationContext, FileManager);
+
+            var searchProductsRequest = new SearchProductsRequest { MetafieldKey = "translate", MetafieldValue="true" };
+
+            var response = await actions.SearchProducts(searchProductsRequest);
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
