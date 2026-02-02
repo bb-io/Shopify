947c2f02988c3a2e3dcc79ebf055fb76c7dfd411
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-02-02 14:15:25 +0200

    add and use EnumExtensions.ToTitle()

--- Apps.Shopify/DataSourceHandlers/Static/ContentTypeDataHandler.cs
+++ Apps.Shopify/DataSourceHandlers/Static/ContentTypeDataHandler.cs
@@ -1,4 +1,5 @@
-﻿using Blackbird.Applications.Sdk.Common.Dynamic;
+﻿using Apps.Shopify.Extensions;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
 
 namespace Apps.Shopify.DataSourceHandlers.DictionaryHandlers;
@@ -9,13 +10,13 @@ public class ContentTypeDataHandler : IStaticDataSourceItemHandler
     {
         return
         [
-            new DataSourceItem(TranslatableResource.COLLECTION.ToString(), TranslatableResource.COLLECTION.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.METAFIELD.ToString(), TranslatableResource.METAFIELD.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.ARTICLE.ToString(), TranslatableResource.ARTICLE.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.BLOG.ToString(), TranslatableResource.BLOG.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.PAGE.ToString(), TranslatableResource.PAGE.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.ONLINE_STORE_THEME.ToString(), TranslatableResource.ONLINE_STORE_THEME.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.PRODUCT.ToString(), TranslatableResource.PRODUCT.ToString().ToLower()),
+            new DataSourceItem(TranslatableResource.COLLECTION.ToString(), TranslatableResource.COLLECTION.ToTitle()),
+            new DataSourceItem(TranslatableResource.METAFIELD.ToString(), TranslatableResource.METAFIELD.ToTitle()),
+            new DataSourceItem(TranslatableResource.ARTICLE.ToString(), TranslatableResource.ARTICLE.ToTitle()),
+            new DataSourceItem(TranslatableResource.BLOG.ToString(), TranslatableResource.BLOG.ToTitle()),
+            new DataSourceItem(TranslatableResource.PAGE.ToString(), TranslatableResource.PAGE.ToTitle()),
+            new DataSourceItem(TranslatableResource.ONLINE_STORE_THEME.ToString(), TranslatableResource.ONLINE_STORE_THEME.ToTitle()),
+            new DataSourceItem(TranslatableResource.PRODUCT.ToString(), TranslatableResource.PRODUCT.ToTitle()),
         ];
     }
 }
--- Apps.Shopify/DataSourceHandlers/Static/PollingContentTypeDataHandler.cs
+++ Apps.Shopify/DataSourceHandlers/Static/PollingContentTypeDataHandler.cs
@@ -1,4 +1,5 @@
-﻿using Blackbird.Applications.Sdk.Common.Dynamic;
+﻿using Apps.Shopify.Extensions;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
 
 namespace Apps.Shopify.DataSourceHandlers.Static;
@@ -9,11 +10,11 @@ public class PollingContentTypeDataHandler : IStaticDataSourceItemHandler
     {
         return
         [
-            new DataSourceItem(TranslatableResource.COLLECTION.ToString(), TranslatableResource.COLLECTION.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.ARTICLE.ToString(), TranslatableResource.ARTICLE.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.BLOG.ToString(), TranslatableResource.BLOG.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.ONLINE_STORE_THEME.ToString(), TranslatableResource.ONLINE_STORE_THEME.ToString().ToLower()),
-            new DataSourceItem(TranslatableResource.PRODUCT.ToString(), TranslatableResource.PRODUCT.ToString().ToLower()),
+            new DataSourceItem(TranslatableResource.COLLECTION.ToString(), TranslatableResource.COLLECTION.ToTitle()),
+            new DataSourceItem(TranslatableResource.ARTICLE.ToString(), TranslatableResource.ARTICLE.ToTitle()),
+            new DataSourceItem(TranslatableResource.BLOG.ToString(), TranslatableResource.BLOG.ToTitle()),
+            new DataSourceItem(TranslatableResource.ONLINE_STORE_THEME.ToString(), TranslatableResource.ONLINE_STORE_THEME.ToTitle()),
+            new DataSourceItem(TranslatableResource.PRODUCT.ToString(), TranslatableResource.PRODUCT.ToTitle()),
         ];
     }
 }
