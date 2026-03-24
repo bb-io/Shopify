05bbca61833bbec21d474998072744aae625bcb0
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-24 17:50:45 +0200

    add IMultiDownloadableContentOutput to ContentUpdatedResponse

--- Apps.Shopify/Models/Response/Content/ContentUpdatedResponse.cs
+++ Apps.Shopify/Models/Response/Content/ContentUpdatedResponse.cs
@@ -1,5 +1,10 @@
 ﻿using Apps.Shopify.Models.Entities.Content;
+using Blackbird.Applications.SDK.Blueprints.Interfaces.CMS;
 
 namespace Apps.Shopify.Models.Response.Content;
 
-public record ContentUpdatedResponse(List<PollingContentItemEntity> Items);
+public record ContentUpdatedResponse(List<PollingContentItemEntity> Items) 
+    : IMultiDownloadableContentOutput<PollingContentItemEntity>
+{
+    public List<PollingContentItemEntity> Items { get; set; } = Items;
+}
