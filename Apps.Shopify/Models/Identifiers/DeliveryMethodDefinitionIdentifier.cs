3e5a3663f99b4ded6966d9cb8018bd90d757ccdc
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-25 10:32:13 +0200

    add missing data source attr in DeliveryMethodDefinitionIdentifier

--- Apps.Shopify/Models/Identifiers/DeliveryMethodDefinitionIdentifier.cs
+++ Apps.Shopify/Models/Identifiers/DeliveryMethodDefinitionIdentifier.cs
@@ -1,9 +1,11 @@
+using Apps.Shopify.DataSourceHandlers;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Shopify.Models.Identifiers;
 
 public class DeliveryMethodDefinitionIdentifier
 {
-    [Display("Delivery method definition ID")]
+    [Display("Delivery method definition ID"), DataSource(typeof(DeliveryMethodDefinitionDataHandler))]
     public string DeliveryMethodDefinitionId { get; set; } = string.Empty;
 }
